85ef135a307e8c942469cefbf439c145c39108cb
Timur Ibragimov <ibragimov.tim02@gmail.com>
2025-11-13 11:15:01 +0300

    feat(api): configure URLs for questions and answers endpoints

--- questions/urls.py
+++ questions/urls.py
@@ -1,5 +1,12 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import QuestionViewSet, AnswerViewSet, create_answer_for_question
+
+router = DefaultRouter()
+router.register(r'questions', QuestionViewSet, basename='questions')
+router.register(r'answers', AnswerViewSet, basename='answers')
 
 urlpatterns = [
-
+    path('', include(router.urls)),
+    path('questions/<int:question_id>/answers/', create_answer_for_question, name='create_answer'),
 ]
